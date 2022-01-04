902f53d73c853a7e695bdc6b415535eef520bc49
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-01-04 12:54:02 +0100

    mon cours

---
+++ 04-fonctions/js/04-01_introduction.js
@@ -0,0 +1,16 @@
+// La fontion random : aléatoire
+
+document.getElementById('p1').innerHTML = Math.floor((Math.random() * 100) + 1);
+
+
+document.getElementById('p2').innerHTML = Math.floor((Math.random() * 11)); /* Ici on écrit * 11 car ns voulons ls nbres de 0 à 10 ce qui équivaut à 11 nbres ! */
+
+document.getElementById('p3').innerHTML = Math.random();
+
+// Fonction personnalisée
+
+function random() {
+    return Math.random() * 100;
+}
+
+document.getElementById('p4').innerHTML = 'Le nombre généré  est ' + randomX100();
