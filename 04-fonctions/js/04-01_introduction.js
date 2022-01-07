fc70c4ae61942d1968de54541e73b887db4f25bc
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-01-07 09:23:18 +0100

    coursJS

--- 04-fonctions/js/04-01_introduction.js
+++ 04-fonctions/js/04-01_introduction.js
@@ -10,10 +10,56 @@ document.getElementById('p3').innerHTML = Math.random();
 // Fonction personnalisée
 /**
  * Multiplication par 100
- * @returns number
+ * @returns float
  */
-function random() {
+function randomX100() {
     return Math.random() * 100;
 }
 
 document.getElementById('p4').innerHTML = `Le nombre généré  est ${randomX100()}`;
+
+// Exemple de fonction personnalisée  avec multiplier
+
+function multiplier(nbr1, nbr2) {
+    return ('Multiplications : ' + nbr1 + 'X' + nbr2 + '=' + (nbr1 * nbr2));
+}
+
+document.querySelector('#p5').innerHTML = multiplier(20, 5);
+
+// Faire la même chose avec addition : 78 et 2 
+
+// Soustraire 45 et 41 / Division 20 et 5 / Modulo 20 et 5
+
+
+
+// Soustraction
+
+function soustraction(nbr1, nbr2) {
+    return ('Soustraction : ' + nbr1 + '-' + nbr2 + '=' + (nbr1 - nbr2));
+}
+
+document.querySelector('#p6').innerHTML = soustraction(45, 41);
+
+// Division
+
+function division(nbr1, nbr2) {
+    return ('Division : ' + nbr1 + '/' + nbr2 + '=' + (nbr1 / nbr2));
+}
+
+document.querySelector('#p7').innerHTML = division(20, 5);
+
+// Modulo
+
+function Modulo(nbr1, nbr2) {
+    return ('Le modulo : ' + nbr1 + '%' + nbr2 + '=' + (nbr1 % nbr2));
+}
+
+document.querySelector('#p8').innerHTML = division(20, 5);
+
+// Addition
+
+function addition(nbr1, nbr2) {
+    return ('Addition : ' + nbr1 + '+' + nbr2 + '=' + (nbr1 + nbr2));
+}
+
+document.querySelector('#p9').innerHTML = multiplier(78, 2);
