e2a258c6dbf8bf0508c035ed5eedf3418abf94ef
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-01-07 13:09:51 +0100

    exojs

--- 00-exos/js/07-parité.js
+++ 00-exos/js/07-parité.js
@@ -6,7 +6,9 @@ for (let i = 1; i <= 10; i++) {
     }
 }
 
-for (let i = 12; i <= 21; i++) {
+let nbUser = parseInt(prompt('Entrez un nombre !'));
+let nbMax = (nbUser + 10);
+for (let i = nbUser; i <= nbMax; i++) {
     if (i % 2 === 0) {
         document.getElementById('p2').innerHTML += i + 'est pair. <br>'
     } else {
@@ -14,12 +16,14 @@ for (let i = 12; i <= 21; i++) {
     }
 }
 
-let i = 12;
-while (i < 21) {
-    i++;
-
-    document.getElementById('p3').innerHTML += i + 'est pair. <br>'
-
-    document.getElementById('p3').innerHTML += i + 'est impair. <br>'
+let nbUser2 = parseInt(prompt('Entrez un nombre !'));
+let nbMax2 = (nbUser2 + 10);
+while (nbUser2 <= nbMax2) {
+    if (nbUser2 % 2 === 0) {
+        document.getElementById('p3').innerHTML += i + 'est pair. <br>'
+    } else {
+        document.getElementById('p3').innerHTML += i + 'est impair. <br>'
+    }
+    nbUser2++;
 
 }
