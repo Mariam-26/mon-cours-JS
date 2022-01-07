e7ef8813c4cd8a999152b9dc9e74bfb1e7014828
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-01-07 16:34:13 +0100

    exojs

--- 00-exos/js/08-saisie.js
+++ 00-exos/js/08-saisie.js
@@ -1,8 +1,8 @@
 let nbrUser = 0;
 
 while (
-    (nbrUser < 50) || (nbrUser > 100)) {
-    nbrUser = prompt('Entrez un nombre entre 50 et 100 !');
+    nbrUser < 50 || nbrUser > 100) {
+    nbrUser = Number(prompt('Saisissez un nombre compris entre 50 et 100 !'));
 
-    document.getElementById('p1').innerHTML = 'votre nombre est entre 50 et 100 ';
+    document.getElementById('p1').innerHTML = `Le nombre récupéré est le ${nbrUser}`;
 }
---
+++ 00-exos/js/09-multiplication.js
@@ -0,0 +1,21 @@
+let nbr = Number(prompt("Mutiplions un nombre"));
+
+for (let i = 1; i <= 10; i++) {
+    let reponse = nbr * i;
+    document.getElementById('p1').innerHTML += `${nbr} X ${i} = ${reponse}<br> `;
+
+    // nbr + ' X ' + i + ' = ' + reponse + ' <br>';
+}
+
+let nbr2 = Number(prompt("Mutiplions un nombre"));
+
+for (let i = 11; i <= 10; i++) {
+    let reponse2 = nbr2 * i;
+    document.getElementById('p2').innerHTML += `${nbr2} X ${i} = ${reponse2}<br> `;
+
+
+
+
+
+    nbr + ' X ' + i + ' = ' + reponse + ' <br>';
+}
