66ec784b0eccc2f1dd0d25a4636362035d55caa8
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-01-08 12:49:16 +0100

    exojs

---
+++ 00-exos/js/exos1-js.js
@@ -0,0 +1,24 @@
+let nom = prompt('Quel est votre nom ?')
+
+let prenom = prompt('Quel est votre prenom ?')
+
+alert(prenom + ' ' + nom);
+
+document.querySelector('#p1').innerHTML = 'Bonjour, vous vous appellez ' + ' ' + nom + ',' + ' ' + prenom;
+
+
+
+
+// let nom = prompt('Veuillez saisir votre nom.')
+// let prenom = prompt('Veuillez saisir votre prenom.')
+// console.log(nom, prenom);
+// alert(prenom +' ' + nom);
+
+// document.querySelector('#p1').innerHTML = 'Bonjour, vous vous appellez ' + ' ' + nom + ',' + ' ' + prenom;
+
+
+// let age = prompt('Quel est votre âge ?')
+
+// let ville = prompt('Dans quelle ville vivez-vous ?')
+
+// document.querySelector('#p2').innerHTML = 'Bonjour, vous avez' + ' ' + age + 'ans et vous habitez à ' + ville + '.';
