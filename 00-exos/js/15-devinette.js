2fd265aeec2001b82e870169937f2f572292c326
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-01-11 15:04:10 +0100

    monjs

--- 00-exos/js/15-devinette.js
+++ 00-exos/js/15-devinette.js
@@ -1,28 +1,38 @@
-// Première : alert
-alert('Binvenue dans le jeu !');
+// 1ere etape : alert
 
-// Deuxième étape : nombre aléatoire
-let nbrAleatoire = Math.floor(Math.random() * 100);
+alert('Bienvenue dans le jeu !');
+
+//2eme étape : nom aléatoire
+
+var nbrAleatoire = Math.floor(Math.random() *
+    100);
 console.log(nbrAleatoire);
 
-// Troixième étape : boucle for
-for (i = 0; i <= 6; i++) {
-    let nbUser = parseInt(prompt(`Trouvez le numéro mystère, compris entre 0 et 100. Attention, vous n'avez que 6 essais.`));
+//3eme etape : boucle for
+
+for (var i = 1; i <= 6; i++) {
+
+    var nbUser = parseInt(prompt('Trouvez le numéro mystère, compris entre 0 et 100. Attention, vous n\'avez que 6 essais'));
     if ((nbUser < 0) || (nbUser > 100)) {
-        alert(`Vous devez saisir un nombre entre 0 et 100 ! ${i} /6`);
-        i--; /* si la personne se trompe ça ne compte pas dans les essais. */
-    } else if (isNaN(nbUser)) {
-        /* isNaN => n'est pas un nombre (isNotNumber) */
-        alert(`Vous devez saisir un NOMBRE ! ${i} /6`);
+
+        alert('vous devez saisir un nombre entre 0 et 100!' + i + '/6');
         i--;
+
+        /*si l'utilisateur se trompe ça ne compte pas dans les essais*/
+    } else if (isNaN(nbUser)) { //* isNan n'est pas un nombre (isNotaNumber)*/
+        alert('Vous devez saisir un NOMBRE ! ' + i + '/6');
+        i--;
+
     } else if (nbrAleatoire < nbUser) {
-        alert(`Visez plus bas ! ${i} /6`);
+        alert('Visez plus bas !' + i + '/6');
+
     } else if (nbrAleatoire > nbUser) {
-        alert(`Visez plus haut ! ${i} /6`);
+        alert('Visez plus haut !' + i + '/6');
     } else {
-        document.getElementById('p1').innerHTML = `Félicitation, vous avez trouvé la réponse ! Le nombre mystère était ${nbrAleatoire}`;
-        break; /* break sert à sortir de la boucle */
+        document.getElementById('p1').innerHTML = 'Félicitation, vous avez trouvé la réponse! Le nombremystère était ' + nbrAleatoire;
+        break; /* break sert à sortir de la boucle*/
     }
-    document.getElementById('p1').innerHTML =
-        `Dommage vous n'avez pas trouvé la bonne réponse, c'était ${nbrAleatoire}`;
-} /* Fin de la boucle aléatoire */
+
+    document.getElementById('p1').innerHTML = 'Dommage vous avez échoué! la bonne réponse ,c\'était ' + nbrAleatoire;
+
+} /* fin de la boucle aléatoir*/
